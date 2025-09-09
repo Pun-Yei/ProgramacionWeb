eac7806ae1cb019b1b959fff14bccee386a47291
Seb-ST <sebastiansiquina11@gmail.com>
2025-09-09 17:10:02 -0600

    feat(models): add Book model and apply migration

--- hw05/models.py
+++ hw05/models.py
@@ -9,3 +9,12 @@ class Autor(models.Model):
     def __str__(self):
         return self.nombre
 
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=100)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    paginas = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.titulo
+
