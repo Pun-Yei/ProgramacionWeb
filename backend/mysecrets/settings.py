9e119aadf4d86ea047e4d91154d82f5de4288480
Seb-ST <sebastiansiquina11@gmail.com>
2025-10-12 15:17:38 -0600

    feat: Integrate Redis cache with Django backend

--- backend/mysecrets/settings.py
+++ backend/mysecrets/settings.py
@@ -102,6 +102,17 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
+# Caché
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",  # Redis corriendo localmente
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
